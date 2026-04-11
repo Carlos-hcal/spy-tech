5bce3cecfc1ebf72d9515474e4443ac558901051
Carlos Cort <carlos.hcal@gmail.com>
2026-04-11 14:12:04 -0300

    feat faqs-section

--- script.js
+++ script.js
@@ -162,3 +162,18 @@ comments.forEach((item) => {
 
   slider.appendChild(clone);
 });
+
+const items = document.querySelectorAll(".faq-item");
+
+items.forEach((item) => {
+  const question = item.querySelector(".faq-question");
+
+  question.addEventListener("click", () => {
+    // fecha outros (opcional)
+    items.forEach(i => {
+      if (i !== item) i.classList.remove("active");
+    });
+
+    item.classList.toggle("active");
+  });
+});
